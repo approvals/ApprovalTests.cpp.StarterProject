a284cafeb8918d17a797e63f24eaf4ff457e5ce8
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-07-31 17:52:16 +0100

    Improve comments for new users
    Co-Authored-By: Llewellyn Falco <isidore@users.noreply.github.com>

--- tests/NewTest.cpp
+++ tests/NewTest.cpp
@@ -1,28 +1,22 @@
 #include "ApprovalTests.hpp"
 #include "Catch.hpp"
-/**
- * In clion, you can run this with ctrl+r
- * You might need to add a Catch Runtime to see it in the unit test window
- * Run > edit configurations > + > Catch > Target=Playground_Tests
- */
+
+// All these tests should pass on your system.
+// Please note that you will need a Diff Tool installed:
+//  https://github.com/approvals/ApprovalTests.cpp/blob/master/doc/Reporters.md#supported-diff-tools
+
 TEST_CASE("ApprovingText")
 {
     Approvals::verify("Approval Tests can verify text via the golder master method ");
-
 }
 
 TEST_CASE("ApprovingWithReporters")
 {
     // you can alter this to make it launch
     Approvals::verify("Reporters launch only on failure, by default they are the DiffReporter", DiffReporter());
-
 }
-
 
 TEST_CASE("Approval Is just an assertion library, you don't need to use it.")
 {
     REQUIRE(std::string("No Approvals, only Catch") == "No Approvals, only Catch");
 }
-
-
-
