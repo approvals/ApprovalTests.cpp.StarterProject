5d325ba7b75679b6fdf6a034dcbd05a6bd39fad8
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-08-12 18:58:51 +0100

    t Update CombinationApprovals for v4.0.0
    Co-Authored-By: Llewellyn Falco <isidore@users.noreply.github.com>

--- tests/DemoTest.cpp
+++ tests/DemoTest.cpp
@@ -24,8 +24,7 @@ TEST_CASE("DemoCombinationTestCase")
 {
     std::vector<int> evens{ 2, 4, 6, 8, 10 };
     std::vector<int> odds{ 1, 3, 5, 7, 9 };
-    CombinationApprovals::verifyAllCombinations<
-        std::vector<int>, std::vector<int>, Demo>(
+    CombinationApprovals::verifyAllCombinations(
             [](int i, int j) { return Demo{ i, j }; },
             evens, odds);
 }
