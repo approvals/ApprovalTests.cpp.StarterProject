dd31115a274d62034a2c35061f2e2428cfca8f3c
Simon Weis <laguna_1989@gmx.net>
2024-03-26 18:58:54 +0100

    . t visual-studio-2019 folder is now machine generated by a script

--- create_visual_studio_project.bat
+++ create_visual_studio_project.bat
@@ -1,5 +1,5 @@
-rmdir /s /q visual-studio-2017
-mkdir       visual-studio-2017
-cd          visual-studio-2017
-cmake -G "Visual Studio 15 2017" ..
+rmdir /s /q visual-studio-2019
+mkdir       visual-studio-2019
+cd          visual-studio-2019
+cmake -G "Visual Studio 16 2019" ..
 cd ..
