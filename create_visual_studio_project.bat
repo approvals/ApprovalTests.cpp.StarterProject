f480fc5b5c605abbb62eea26dff6dd5b572a0aac
Simon Weis <laguna_1989@gmx.net>
2024-03-26 19:20:34 +0100

    . e switch to windows-latest

--- create_visual_studio_project.bat
+++ create_visual_studio_project.bat
@@ -1,5 +1,5 @@
-rmdir /s /q visual-studio-2019
-mkdir       visual-studio-2019
-cd          visual-studio-2019
-cmake -G "Visual Studio 16 2019" ..
+rmdir /s /q visual-studio
+mkdir       visual-studio
+cd          visual-studio
+cmake ..
 cd ..
